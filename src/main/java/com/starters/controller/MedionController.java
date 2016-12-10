1a2b4b69e9cd8f19ec668189180c50ea334b27c9
KeerthiTejaN <keerthi.nuthi@gmail.com>
2016-12-09 20:06:27 -0500

    Medion Calculation

--- src/main/java/com/starters/controller/MedionController.java
+++ src/main/java/com/starters/controller/MedionController.java
@@ -9,25 +9,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.starters.inf.AddUserEventInterface;
 import com.starters.model.Coordinates;
+import com.starters.model.Eid;
 import com.starters.model.UserEvent;
 import com.starters.service.CalculateMedianService;
+import com.starters.service.FcmNotificationService;
 
 
+@RestController
 public class MedionController {
 	
 	@Autowired
 	AddUserEventInterface addUserEventInterface;
 	
-	@Autowired
+//	@Autowired
 	Coordinates coordinate;
 	
-	@Autowired
-	CalculateMedianService calculateMedionService;
-	
-	UserEvent userEvent;
+//	@Autowired
+	private CalculateMedianService calculateMedionService;
+	private FcmNotificationService fcmNotificationService;
+	private UserEvent userEvent;
 	
 	public <UserEvent> ArrayList<UserEvent> makeCollection(Iterable<UserEvent> iter) {
     ArrayList<UserEvent> list = new ArrayList<UserEvent>();
@@ -35,18 +39,28 @@ public class MedionController {
         list.add(item);
     }
     return list;
-}
+	}
+	
+	public List<UserEvent> findSpecificUserEvents(int eventId)
+	{
+		List<UserEvent> userEventList = new ArrayList<UserEvent>();
+		for(UserEvent userEvent: addUserEventInterface.findAll()){
+			if(userEvent.getEventId() == eventId){
+				userEventList.add(userEvent);
+			}
+		}
+		return userEventList;
+	}
 
 	@RequestMapping(value="/api/calcMedian", method=RequestMethod.POST, produces=MediaType.TEXT_PLAIN_VALUE)
-	public @ResponseBody String calcMedian(@RequestBody String eventID){
-		int eventid = Integer.parseInt(eventID);
-		System.out.println("EventID "+eventid);
+	public @ResponseBody String calcMedian(@RequestBody Eid eid){
+		fcmNotificationService = new FcmNotificationService();
+		coordinate = new Coordinates();
+		calculateMedionService = new CalculateMedianService();
+		int eventid = eid.getId();
 		List<Coordinates> coordinateList = new ArrayList<Coordinates>();
-		List<Integer> input = new ArrayList<Integer>();
-		input.add(eventid);
-		addUserEventInterface.findAll(input);
-		ArrayList<UserEvent> userEvents;
-		userEvents = makeCollection(addUserEventInterface.findAll());
+		List<UserEvent> userEvents;
+		userEvents = findSpecificUserEvents(eventid);
 		System.out.println("Number of members:"+userEvents.size());
 		for(int i=0; i<userEvents.size(); i++){
 			coordinate.setLatitude(Double.parseDouble(userEvents.get(i).getLatitude()));
@@ -56,7 +70,12 @@ public class MedionController {
 		coordinate = calculateMedionService.getMedian(coordinateList);
 		System.out.println(coordinate.getLatitude()+" : "+coordinate.getLongitude());
 		
+		for(int i=0; i<userEvents.size(); i++){
+			fcmNotificationService.notify("MedionCalculated,"+Double.toString(coordinate.getLatitude())+","+Double.toString(coordinate.getLongitude()), userEvents.get(i).getUserFcmToken());
+		}
+		
 		return "MedianCalculated";
+		
 	}
 	
 }
---
+++ src/main/java/com/starters/model/Eid.java
@@ -0,0 +1,15 @@
+package com.starters.model;
+
+public class Eid {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
